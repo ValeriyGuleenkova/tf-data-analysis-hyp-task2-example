6939a3ff4564a4669493344cda0a50e56494edd1
ValeriyGuleenkova <64977472+ValeriyGuleenkova@users.noreply.github.com>
2023-04-11 23:14:53 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import anderson_ksamp
 
+chat_id = 215606022 # Ваш chat ID, не меняйте название переменной
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+SGN_LVL = 0.07
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    pval = anderson_ksamp([x, y])[2]
+    if pval < SGN_LVL:
+        return True
+    # true: выборки не равны
+    return False
